b967da2e6431ae1f7731f9b64a7c5690ab9853f5
jamaspy <aspin@me.com>
2022-03-11 09:03:28 +1100

    Edit menu

---
+++ src/components/Menu/Bubbly.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+const Bubbly = ({ menuData }) => {
+  return (
+    <div className="shadow flex-1 text-center rounded-md mx-1 bg-offWhite p-4 mt-2">
+      <div className="flex items-center justify-center">
+        <StaticImage src="../../images/drinks_2.png" width={100} alt="Wine" />
+      </div>
+      <h1 className="text-center">Bubbles</h1>
+      {menuData &&
+        menuData.map(wine => (
+          <div className="w-full flex justify-between flex-row">
+            <div className="flex flex-col items-start justify-start">
+              <p>{wine.name}</p>
+              <div className="flex flex-row text-xs">
+                <p className="mr-2">{wine.region}</p>
+                <p>{wine.grape}</p>
+              </div>
+            </div>
+            <p>
+              {wine.glass_price || " "} | {wine.bottle_price || " "}
+            </p>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default Bubbly
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -7,54 +7,9 @@ import * as styles from "../styles/modules/menu.module.css"
 import MenuList from "../components/Menu"
 
 const Menu = () => {
-  const data = useStaticQuery(graphql`
-    {
-      web: allFile(filter: { name: { eq: "mixed" } }) {
-        nodes {
-          name
-          childImageSharp {
-            gatsbyImageData(
-              quality: 100
-              placeholder: TRACED_SVG
-              formats: [AUTO, WEBP, AVIF]
-              layout: FULL_WIDTH
-            )
-          }
-        }
-      }
-      menu: allContentfulFoodMenuMenuJsonNode {
-        nodes {
-          cheeses {
-            name
-            price
-          }
-          meats {
-            price
-            name
-          }
-          plates {
-            name
-            price
-          }
-          sides {
-            name
-            price
-          }
-          toasties_and_baguettes {
-            name
-            price
-          }
-        }
-      }
-    }
-  `)
-  const heroData = data.web.nodes[0].childImageSharp.gatsbyImageData
-  const menuData = data.menu.nodes[0]
-  console.log("🚀  TESTY:  : Menu : menuData", menuData)
   return (
     <Layout>
       <SEO title="Menu" />
-
       <div className={styles.main_backgrond}>
         <MenuList />
       </div>
