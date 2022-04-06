f55680433cf61d166ecc04ba715cbcb9285eb77f
jamaspy <aspin@me.com>
2022-04-06 16:23:34 +1000

    Rename SEO import

--- src/pages/whatson.tsx
+++ src/pages/whatson.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 import WhatsOnList from "../components/WhatsOn"
 const WhatsOn = () => {
   return (
     <Layout>
-      <SEO title="What's On" />
+      <Seo title="What's On" />
       <WhatsOnList />
     </Layout>
   )
